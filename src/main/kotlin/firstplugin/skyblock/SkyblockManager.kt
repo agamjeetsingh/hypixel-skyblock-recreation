5b6449bcf6291c8d640b75fa3381a0a32176548f
agamjeetsingh <agamjeetsingh2005@gmail.com>
2025-03-25 10:54:57 +0000

    Added adhoc event handlers

--- src/main/kotlin/firstplugin/skyblock/SkyblockManager.kt
+++ src/main/kotlin/firstplugin/skyblock/SkyblockManager.kt
@@ -1,12 +1,19 @@
 package firstplugin.skyblock
 
 import firstplugin.plugin.SkyblockPlugin
-import firstplugin.skyblock.database.PlayerDataService
+import firstplugin.skyblock.attributes.sbHealth
+import firstplugin.skyblock.database.JsonPlayerDataService
 import firstplugin.skyblock.menu.MenuManager
+import org.bukkit.Material
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.inventory.ItemStack
 
 /**
  * The SkyblockManager class stores all the properties of online players in
@@ -16,11 +23,15 @@ import org.bukkit.event.player.PlayerQuitEvent
 class SkyblockManager(
     private val plugin: SkyblockPlugin,
 ) : Listener {
-    private val playerDataService = PlayerDataService(plugin)
+    private val playerDataService = JsonPlayerDataService(plugin)
     private val menuManager = MenuManager(plugin)
 
     // Map of online SkyblockPlayer
     private val players = mutableMapOf<String, SkyblockPlayer>()
+
+    // Get all online SkyblockPlayers
+    val onlinePlayers: Collection<SkyblockPlayer>
+        get() = players.values
 
     fun initialize() {
         // Register this manager as an event listener
@@ -51,6 +62,8 @@ class SkyblockManager(
 
         // Load player data from database
         playerDataService.loadPlayerDataAsync(player)
+
+        player.startRegen(plugin)
     }
 
     /**
@@ -69,5 +82,26 @@ class SkyblockManager(
      */
     fun saveAllPlayers() {
         playerDataService.saveAllPlayers(players.values)
+    }
+
+    @EventHandler
+    fun onPlayerEatGoldenApple(event: PlayerItemConsumeEvent) {
+        val player = players[event.player.name]!!
+        if (event.item == ItemStack(Material.GOLDEN_APPLE)) {
+            player.sbHealth.absorption += 10.0
+        }
+    }
+
+    @EventHandler
+    fun onPlayerDamage(event: EntityDamageEvent) {
+        val entity: Entity = event.entity
+        if (entity is Player) {
+            val player = players[entity.name]!!
+            // TODO - It would be more natural to have player.damage()
+            player.sbHealth.damage(event.damage * 5.0)
+            player.bukkitPlayer.absorptionAmount = player.sbHealth.absorption / 5.0
+            player.bukkitPlayer.health =
+                (player.sbHealth.current) * (player.bukkitPlayer.maxHealth) / (player.sbHealth.max)
+        }
     }
 }
