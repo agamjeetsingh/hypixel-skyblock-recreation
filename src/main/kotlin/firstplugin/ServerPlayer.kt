503a6ad75190b2d835ba991cf49dda870fb90846
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-06-16 13:01:44 +0100

    Removed serialisation

--- src/main/kotlin/firstplugin/ServerPlayer.kt
+++ src/main/kotlin/firstplugin/ServerPlayer.kt
@@ -1,9 +1,7 @@
 package firstplugin
 
-import kotlinx.serialization.Serializable
 import org.bukkit.entity.Player
 
-@Serializable
 class ServerPlayer(
     val bukkitPlayer: Player,
     var rank: Rank = Rank.DEFAULT,
