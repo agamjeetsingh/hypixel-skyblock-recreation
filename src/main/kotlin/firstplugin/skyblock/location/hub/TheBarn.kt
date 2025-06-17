2bee719d60264daf07df74358a51d1566157b3a8
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-06-17 19:30:49 +0100

    Implemented TheBarn class, a Zone

---
+++ src/main/kotlin/firstplugin/skyblock/location/hub/TheBarn.kt
@@ -0,0 +1,35 @@
+package firstplugin.skyblock.location.hub
+
+import firstplugin.skyblock.location.Zone
+import firstplugin.skyblock.utils.SkyblockLore
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+
+class TheBarn : Zone() {
+    override val name: String = "The Barn"
+    override val color: NamedTextColor = NamedTextColor.AQUA
+    override val questLines: SkyblockLore
+        get() {
+            val skyblockLore = SkyblockLore()
+            skyblockLore.addLore(
+                Component
+                    .text("$BULLET_POINT Harvest Wheat, Carrots, Potatoes, Pumpkins, and Melons.")
+                    .color(NamedTextColor.WHITE)
+                    .decoration(TextDecoration.ITALIC, false),
+            )
+            skyblockLore.addLore(
+                Component
+                    .text("$BULLET_POINT Kill cows, chickens, and pigs.")
+                    .color(NamedTextColor.WHITE)
+                    .decoration(TextDecoration.ITALIC, false),
+            )
+            skyblockLore.addLore(
+                Component
+                    .text("$BULLET_POINT Milk cows.")
+                    .color(NamedTextColor.WHITE)
+                    .decoration(TextDecoration.ITALIC, false), // TODO: Replace The Barn with the actual class
+            )
+            return skyblockLore
+        }
+}
