44254a07c59bb8725c4f436da6798847aa5f9626
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-06-17 19:42:45 +0100

    Implemented AuctionHouse class (Zone)

---
+++ src/main/kotlin/firstplugin/skyblock/location/hub/AuctionHouse.kt
@@ -0,0 +1,29 @@
+package firstplugin.skyblock.location.hub
+
+import firstplugin.skyblock.location.Zone
+import firstplugin.skyblock.utils.SkyblockLore
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+
+class AuctionHouse : Zone() {
+    override val name: String = "Auction House"
+    override val color: NamedTextColor = NamedTextColor.GOLD
+    override val questLines: SkyblockLore
+        get() {
+            val skyblockLore = SkyblockLore()
+            skyblockLore.addLore(
+                Component
+                    .text("Auction off your special items.")
+                    .color(NamedTextColor.WHITE)
+                    .decoration(TextDecoration.ITALIC, false),
+            )
+            skyblockLore.addLore(
+                Component
+                    .text("Bid on other players' items.")
+                    .color(NamedTextColor.WHITE)
+                    .decoration(TextDecoration.ITALIC, false),
+            )
+            return skyblockLore
+        }
+}
