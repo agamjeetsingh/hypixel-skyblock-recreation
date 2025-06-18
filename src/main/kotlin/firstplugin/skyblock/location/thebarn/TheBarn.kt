0db76e58d4a7729c33d5abd5034e83457242de82
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-06-18 11:20:26 +0100

    Made TheBarn an object, and a SkyblockLocation and added skillRequirements and zones

--- src/main/kotlin/firstplugin/skyblock/location/thebarn/TheBarn.kt
+++ src/main/kotlin/firstplugin/skyblock/location/thebarn/TheBarn.kt
@@ -1,12 +1,15 @@
-package firstplugin.skyblock.location.hub
+package firstplugin.skyblock.location.thebarn
 
+import firstplugin.skyblock.location.SkyblockLocation
 import firstplugin.skyblock.location.Zone
+import firstplugin.skyblock.skill.FarmingSkill
+import firstplugin.skyblock.skill.Skill
 import firstplugin.skyblock.utils.SkyblockLore
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
 
-class TheBarn : Zone() {
+object TheBarn : SkyblockLocation() {
     override val name: String = "The Barn"
     override val color: NamedTextColor = NamedTextColor.AQUA
     override val questLinesWithoutBulletPoints: SkyblockLore
@@ -32,4 +35,10 @@ class TheBarn : Zone() {
             )
             return skyblockLore
         }
+    override val skillRequirements: List<Skill.Requirement<Skill>> =
+        listOf(
+            Skill.Requirement(FarmingSkill::class, 1),
+        )
+
+    override val zones: List<Zone> = listOf()
 }
