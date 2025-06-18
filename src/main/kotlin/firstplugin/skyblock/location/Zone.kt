c7545b9e25215b4722f24a0aaeaaec0c5e1784a2
agamjeetsingh <agamjeet.singh24@imperial.ac.uk>
2025-06-18 11:08:57 +0100

    Made SkyblockArea invoke constructor since now a abstract class and not an interface

--- src/main/kotlin/firstplugin/skyblock/location/Zone.kt
+++ src/main/kotlin/firstplugin/skyblock/location/Zone.kt
@@ -5,6 +5,6 @@ package firstplugin.skyblock.location
  * unlike Zones, Locations have skill requirements. So if a player fulfills the skill requirement of
  * a Location, then they can access any Zone inside that Location
  */
-abstract class Zone : SkyblockArea {
+abstract class Zone : SkyblockArea() {
     abstract val location: SkyblockLocation
 }
